ebde468158b8bded56d127f2246d78f2b42ecfdb
EinJOJO <schlotmax507@gmail.com>
2024-03-28 19:19:58 +0100

    Implemnented interface in DummyServerFactory

--- core/src/test/java/DummyServerFactory.java
+++ core/src/test/java/DummyServerFactory.java
@@ -1,4 +1,5 @@
 import it.einjojo.nucleoflex.api.impl.ImplServer;
+import it.einjojo.nucleoflex.api.server.ProxyServer;
 import it.einjojo.nucleoflex.api.server.Server;
 import it.einjojo.nucleoflex.server.ServerFactory;
 
@@ -6,5 +7,10 @@ public class DummyServerFactory implements ServerFactory {
     @Override
     public Server createServer(String serverName, String groupName) {
         return new ImplServer(serverName, groupName, null, null, null);
+    }
+
+    @Override
+    public ProxyServer createProxyServer(String proxyName) {
+        return null;
     }
 }
